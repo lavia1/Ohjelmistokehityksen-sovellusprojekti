0d6c60f5a4ff52d57a9e70ad22a3280bd82f0356
lavia1 <lauraviide02@gmail.com>
2026-01-18 11:58:30 +0200

    Autojen CRUD-operaatiot sekä testaus Postmanissa

--- RestApi/index.js
+++ RestApi/index.js
@@ -14,7 +14,7 @@ app.post("/books", (req, res) => {
   const sql = "INSERT INTO book (name, author, isbn) VALUES (?, ?, ?)";
   db.query(sql, [name, author, isbn], (err, result) => {
     if (err) res.status(500).json(err);
-    else res.json({ message: "Book added", id: result.insertId });
+    else res.json({ message: "Kirja lisätty", id: result.insertId });
   });
 });
 
@@ -33,7 +33,7 @@ app.put("/books/:id", (req, res) => {
   const sql = "UPDATE book SET name=?, author=?, isbn=? WHERE id_book=?";
   db.query(sql, [name, author, isbn, id], (err) => {
     if (err) res.status(500).json(err);
-    else res.json({ message: "Book updated" });
+    else res.json({ message: "Kirja päivitetty" });
   });
 });
 
@@ -42,7 +42,46 @@ app.delete("/books/:id", (req, res) => {
   const id = req.params.id;
   db.query("DELETE FROM book WHERE id_book=?", [id], (err) => {
     if (err) res.status(500).json(err);
-    else res.json({ message: "Book deleted" });
+    else res.json({ message: "Kirja poistettu" });
+  });
+});
+
+// Autojen CRUD-operaatiot
+// CREATE
+app.post("/cars", (req, res) => {
+  const { branch, model } = req.body;
+  const sql = "INSERT INTO car (branch, model) VALUES (?, ?)";
+  db.query(sql, [branch, model], (err, result) => {
+    if (err) res.status(500).json(err);
+    else res.json({ message: "Auto lisätty", id: result.insertId });
+  });
+});
+
+// READ / GET
+app.get("/cars", (req, res) => {
+  db.query("SELECT * FROM car", (err, rows) => {
+    if (err) res.status(500).json(err);
+    else res.json(rows);
+  });
+});
+
+// UPDATE
+app.put("/cars/:id", (req, res) => {
+  const { branch, model } = req.body;
+  const id = req.params.id;
+  const sql = "UPDATE car SET branch=?, model=? WHERE id_car=?";
+  db.query(sql, [branch, model, id], (err) => {
+    if (err) res.status(500).json(err);
+    else res.json({ message: "Auto päivitetty" });
+  });
+});
+
+// DELETE
+app.delete("/cars/:id", (req, res) => {
+  const id = req.params.id;
+  db.query("DELETE FROM car WHERE id_car=?", [id], (err) => {
+    if (err) res.status(500).json(err);
+    else res.json({ message: "Auto poistettu" });
   });
 });
 
